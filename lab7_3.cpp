66281a159555a32d43875fe9363b775cb7b2583f
jaonayzzz <pakornpat9595@gmail.com>
2025-12-13 22:57:43 +0700

    completed

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,8 +1,16 @@
 #include<iostream>
 using namespace std;
-
 char before(char x){
-	//Write your function definition here
+   if(x>='A'&&x<='Z'){
+        if(x=='A'){
+        return 'Z';
+        }
+        else{
+            return char(x - 1);
+        }
+    }
+    else{return '0';}
+    
 }
 
 int main(){
